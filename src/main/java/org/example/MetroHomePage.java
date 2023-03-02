95b8cd8a93ace99b8d3f1b48fb5d204d3566952f
Nikita Kompanits <kompanitsnikita@gmail.com>
2023-03-02 15:31:20 +0400

    UpdateTwo

--- src/main/java/org/example/MetroHomePage.java
+++ src/main/java/org/example/MetroHomePage.java
@@ -28,4 +28,31 @@ public class MetroHomePage {
     // локатор коллеций станций "откуда" и "куда" маршрута по имени класса
     @FindBy(how = How.CLASS_NAME, using = "route-details-block__terminal-station")
     private ElementsCollection routeStationFromTo;
+
+    // метод ожидания загрузки страницы: проверка видимости станции "театральная"
+    public void waitForLoadHomePage() {
+        // находим элемент по тексу, добавляем ожидание 8 секунд
+
+        $(byText("Театральная")).shouldBe(visible,ofSeconds(8));
+    }
+
+    // метод выбора города по названию
+    public void chooseCity(String cityName){
+        selectCityButton.click();
+        // выбор нужного города
+            $(byText(cityName)).click();
+    }
+
+    // метод ввода названия станции в поле "Откуда"
+    public void setStationFrom(String stationFrom) {
+        // ввод названия станции в поле ввода, затем с помощью клавищ "Вниз" и enter выбрали его в выпадающем списке
+        fieldFrom.setValue(stationFrom).sendKeys(Keys.DOWN, Keys.ENTER);
+    }
+    // метод ожидания построения маршрута: проверяется видимость кнопки "Получить ссылку на маршрут"
+    public void waitForLoadRoute(){
+        // ищем веб-элемент по тексту
+        $( byText("Получить ссылку на маршрут")).shouldBe(visible);
+    }
+
+
 }
