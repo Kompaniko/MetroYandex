ef41cf1459deb3ed359f4634b4db6c85204bdf15
Nikita Kompanits <kompanitsnikita@gmail.com>
2023-03-02 15:52:27 +0400

    UpdateThree

---
+++ src/test/java/SelenideMetroTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideMetroTest {
+
+}
